9438c9b4327403ad8c34ddf6819169249bc737c4
Lev <lev-titov@mail.ru>
2018-05-04 14:04:46 +0300

    Add another arrray2

--- src/CompareTimeSorting.java
+++ src/CompareTimeSorting.java
@@ -4,6 +4,7 @@ public class CompareTimeSorting {
         Array array = new Array(100000);
         Array array1 = new Array(array);
         Array array2 = new Array(array);
+        Array array2 = new Array(array);
         array.bubbleSort();
         array1.selectSort();
         array2.insertSort();
